5cb0ea872bb0f7264cef4add4b8bab1244ee809f
Joseph Nasser <jnasser@broadinstitute.org>
2017-02-23 00:24:29 -0500

    Adding eps hack to avoid dividing by zero

--- calculate_wtd_avg_wts_expression.m
+++ calculate_wtd_avg_wts_expression.m
@@ -1,28 +1,31 @@
-function wts = calculate_wtd_avg_wts_expression(expr,target_line,pred_line,gene_name)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+function wts = calculate_wtd_avg_wts_expression(expr_table,target_line,pred_line,gene_name)
+%calculates the weights to be used in a weighted average computation given
+%a target line and a set of predictor lines. Based on baseline expression.
 
-gene_idx = strcmp(expr{:,'gene_name'},gene_name);
-target_expr = expr{gene_idx,lower(sprintf('%s_avg_FPKM',target_line))};
+gene_idx = strcmp(expr_table{:,'gene_name'},gene_name);
+%target_expr = expr_table{gene_idx,lower(sprintf('%s_avg_FPKM',target_line))};
+target_expr = expr_table{gene_idx,target_line};
 
-cellfun(@(s) sprintf('%s_avg_FPKM',s),pred_line,'Uni',false)
+%cellfun(@(s) sprintf('%s_avg_FPKM',s),pred_line,'Uni',false)
 
-predictor_idx = ismember(lower(expr.Properties.VariableNames),...
-    lower(cellfun(@(s) sprintf('%s_avg_FPKM',s),pred_line,'Uni',false)));
+% predictor_idx = ismember(lower(expr_table.Properties.VariableNames),...
+%     lower(cellfun(@(s) sprintf('%s_avg_FPKM',s),pred_line,'Uni',false)));
 
-pred_expr = expr{gene_idx,predictor_idx};
+predictor_idx = ismember(expr_table.Properties.VariableNames,pred_line);
 
-wts_temp = 1 ./ abs(target_expr - pred_expr);
+pred_expr = expr_table{gene_idx,predictor_idx};
+
+wts_temp = 1 ./ abs(target_expr - pred_expr + eps);
 wts_temp = wts_temp ./ sum(wts_temp);
 
 wts = struct([]);
 
 for ii = 1:length(pred_line)
-    this_line = sprintf('%s_avg_FPKM',pred_line{ii});
-    idx = strcmpi(this_line,expr.Properties.VariableNames(predictor_idx));
+    %this_line = sprintf('%s_avg_FPKM',pred_line{ii});
+    this_line = pred_line{ii};
+    idx = strcmpi(this_line,expr_table.Properties.VariableNames(predictor_idx));
     wts(ii).cell_line = pred_line{ii};
     wts(ii).wt = wts_temp(idx);
-    
 end
 
 
